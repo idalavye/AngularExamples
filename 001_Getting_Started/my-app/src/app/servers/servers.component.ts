d8685061d6970c42da948f795482568c34806ca5
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-11-18 16:31:19 +0300

    i learned data binding with using string interpolation

---
+++ 001_Getting_Started/my-app/src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector:'[app-servers]',
+  // selector:'.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  //template:'<app-server></app-server><app-server></app-server>', //İnline template eğer 3 satırdan daha az html kodumuz varsa bu şekilde template oluşturabiliriz.
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
